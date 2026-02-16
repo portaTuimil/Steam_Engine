8ebc02150bcc49df5e9033f12e43ce5e652d004f
Pablo Porta <pabloportatuimil12@gmail.com>
2026-02-16 09:47:02 +0100

    Feature: first part of the coded refactored to classes.

--- src/index.js
+++ src/index.js
@@ -1,105 +1,272 @@
 const canvas = document.getElementById("steam_engine");
-const ctx = canvas.getContext("2d");
+const lineColor = "white";
+const fillColor = "white";
+const lineWidth = 10;
 
-canvas.width = window.innerWidth/ 1.2;
-canvas.height = (window.innerWidth/ 1.2)*9/16;
+window.addEventListener("load", () => {
+    const width = window.innerWidth / 1.2;
+    const height = width * 9 / 16;
+    canvas.width = width;
+    canvas.height = height;
+    const steamEngine = new SteamEngine(canvas, lineColor, fillColor, lineWidth);
+});
 
-const linewWidth = 10;
+window.addEventListener("resize", () => {
+    const canvas = document.getElementById("steam_engine");
+    const width = window.innerWidth / 1.2;  //proportions of the viewport
+    const height = width * 9 / 16;
 
-ctx.fillStyle = "white";
-ctx.strokeStyle = "white";
-ctx.lineWidth = linewWidth;
-
-let angle = 0;
-
-function draw(angle){
-    drawWheel(angle);
-    drawPiston(angle);
-    drawCombustionChamber();
-    drawBase();
-    drawPistonValve(angle);
-    drawTopBlock();
-    drawExhaust();
-}
+    canvas.steamEngineInstance.resize(width, height);
+});
 
 
-const wheel_x = 1070;
-const wheel_y = 600;
-const wheel_ext_r = 240;
-const wheel_piston_r = 100;
-const spoke_n = 7;
-const spoke_a = (360 / spoke_n)*Math.PI/180;
-const spoke_w = 8;    
-const spoke_curve = 0.15 * wheel_ext_r;
 
-function drawWheel(angle){
-    ctx.beginPath();
-    ctx.lineWidth = 22;
-    ctx.arc(wheel_x, wheel_y, wheel_ext_r, 0, 2 * Math.PI);
-    ctx.stroke();
+class SteamEngine {
+    constructor(canvas, lineColor, fillColor, lineWidth) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
 
-    ctx.beginPath();
-    ctx.lineWidth = 6;
-    ctx.arc(wheel_x, wheel_y, wheel_piston_r, 0, 2 * Math.PI);
-    ctx.stroke();
+        this.width = canvas.width;
+        this.height = canvas.height;
 
-    for (let i = 0; i < 10; i++) {
-        const a = angle + i * spoke_a;
-        const endX = wheel_x + wheel_ext_r * Math.cos(a);
-        const endY = wheel_y + wheel_ext_r * Math.sin(a);
-        // midpoint of spoke
-        const midX = wheel_x + 0.5 * wheel_ext_r * Math.cos(a);
-        const midY = wheel_y + 0.5 * wheel_ext_r * Math.sin(a);
-        // perpendicular direction
-        const perpX = -Math.sin(a);
-        const perpY =  Math.cos(a);
-        // control point
-        const ctrlX = midX + perpX * spoke_curve;
-        const ctrlY = midY + perpY * spoke_curve;
-        ctx.beginPath();
-        ctx.lineWidth = spoke_w;
-        ctx.lineCap = "butt";
-        ctx.moveTo(wheel_x, wheel_y);
-        ctx.quadraticCurveTo(ctrlX, ctrlY, endX, endY);
-        ctx.stroke();
+        this.lastTime = 0;
+        this.speed = (100 * Math.PI) / 180;
+        this.model = new SteamModel(this.angle, this.width, this.height);
+        this.animate = this.animate.bind(this);
+        this.canvas.steamEngineInstance = this;
+
+        this.lineColor = lineColor;
+        this.fillColor = fillColor;
+        this.lineWidth = lineWidth;
+        this.setDefaultStyles();
+        requestAnimationFrame(this.animate);
     }
-    ctx.lineWidth = linewWidth;
+
+    draw() {
+        this.drawWheel();
+        this.drawPiston();
+        this.drawChamber();
+    }
+
+    animate(time) {
+        const deltaTime = (time - (this.lastTime || time)) / 1000;
+        this.lastTime = time;
+        const deltaAngle = this.speed * deltaTime;
+
+        this.model.calculate(deltaAngle);
+
+        this.ctx.clearRect(0, 0, this.width, this.height);
+
+        this.ctx.save();
+        this.ctx.scale(this.model.scale, this.model.scale);
+        this.ctx.translate(-40, 0);
+
+        this.draw();
+        this.ctx.restore();
+
+        requestAnimationFrame(this.animate);
+    }
+
+    resize(width, height, lineColor, fillColor, lineWidth) {
+        this.width = width;
+        this.height = height;
+
+        this.canvas.width = width;
+        this.canvas.height = height;
+
+        this.model.resize(width, height);
+
+        this.setDefaultStyles();
+    }
+
+    setDefaultStyles(){
+        this.ctx.strokeStyle = this.lineColor;
+        this.ctx.fillStyle = this.fillColor;
+        this.ctx.lineWidth = this.lineWidth;
+    }
+
+    drawWheel(){
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 22;
+        this.ctx.arc(this.model.wheelX, this.model.wheelY, this.model.wheelExteriorRadius, 0, 2 * Math.PI);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 6;
+        this.ctx.arc(this.model.wheelX, this.model.wheelY, this.model.wheelPistonRadius, 0, 2 * Math.PI);
+        this.ctx.stroke();
+
+        for (let i = 0; i < 10; i++) {
+            const a = this.model.angle + i * this.model.spokeAngle;
+            const endX = this.model.wheelX + this.model.wheelExteriorRadius * Math.cos(a);
+            const endY = this.model.wheelY + this.model.wheelExteriorRadius * Math.sin(a);
+            // midpoint of spoke
+            const midX = this.model.wheelX + 0.5 * this.model.wheelExteriorRadius * Math.cos(a);
+            const midY = this.model.wheelY + 0.5 * this.model.wheelExteriorRadius * Math.sin(a);
+            // perpendicular direction
+            const perpX = -Math.sin(a);
+            const perpY =  Math.cos(a);
+            // control point
+            const ctrlX = midX + perpX * this.model.spokeCurve;
+            const ctrlY = midY + perpY * this.model.spokeCurve;
+            this.ctx.beginPath();
+            this.ctx.lineWidth = this.model.spokeLineWidth;
+            this.ctx.lineCap = "butt";
+            this.ctx.moveTo(this.model.wheelX, this.model.wheelY);
+            this.ctx.quadraticCurveTo(ctrlX, ctrlY, endX, endY);
+            this.ctx.stroke();
+        };
+
+        this.ctx.lineWidth = this.linewWidth;
+    }
+
+    drawPiston(){
+        this.ctx.beginPath();
+        this.ctx.lineCap = "round";
+        this.ctx.moveTo(this.model.wheelArmX, this.model.wheelArmY);
+        this.ctx.lineTo(this.model.pistonRodX, this.model.pistonY);
+        this.ctx.stroke();
+        
+        this.ctx.beginPath(); //pistonRod and piston
+        this.ctx.fillRect(this.model.pistonX + this.model.pistonWidth/2, this.model.pistonY - this.model.pistonRodHeight/2, this.model.pistonRodWidth, this.model.pistonRodHeight);
+        this.ctx.fillRect(this.model.pistonX - this.model.pistonWidth/2, this.model.pistonY - this.model.pistonHeight / 2, this.model.pistonWidth, this.model.pistonHeight); 
+        this.ctx.fill();
+    }
+
+    drawChamber(){
+        this.ctx.beginPath(); //lower combustion chamber
+        this.ctx.lineCap = "butt";
+        this.ctx.moveTo(this.model.rightChamberWall, this.model.pistonY + this.model.rodGap);
+        this.ctx.lineTo(this.model.rightChamberWall, this.model.pistonY + this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.leftChamberWall, this.model.pistonY + this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.leftChamberWall, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.model.rightChamberWall, this.model.pistonY - this.model.rodGap);   
+        this.ctx.lineTo(this.model.rightChamberWall, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();//middle block exterior left
+        this.ctx.moveTo(this.model.leftChamberWall + this.model.bezierPipeX3 + 20, this.model.bezierPipeY3 - 15);
+        this.ctx.lineTo(this.model.rightChamberWall - this.model.bezierPipeX3 - 20, this.model.bezierPipeY3 - 15);
+        this.ctx.strokeStyle ="#FFFFFF07";
+        this.ctx.stroke();
+
+        this.ctx.beginPath();//middle block right
+        this.ctx.strokeStyle =this.lineColor;
+        this.ctx.lineCap = "butt";
+        this.ctx.moveTo(this.model.middlePoint - 10, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.pmiX, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.pmiX, this.model.pistonY - this.model.pistonHeight/2 - this.model.bezierPadding1);
+        this.ctx.bezierCurveTo(this.model.pmiX - this.model.bezierPipeX1, this.model.bezierPipeY1, this.model.pmiX, this.model.bezierPipeY2, this.model.pmiX - this.model.bezierPipeX3, this.model.bezierPipeY3);
+        this.ctx.lineTo(this.model.pmiX - this.model.bezierPipeX3 - 5, this.model.bezierPipeY3);
+        this.ctx.lineTo(this.model.pmiX - this.model.bezierPipeX3 - 5, this.model.bezierPipeY3 - 15);
+        this.ctx.lineTo(this.model.pmiX - this.model.bezierPipeX3 - 15, this.model.bezierPipeY3 - 15);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();//middle block left
+        this.ctx.moveTo(this.model.middlePoint - 10, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.pmsX, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.pmsX, this.model.pistonY - this.model.pistonHeight/2 - this.model.bezierPadding1);
+        this.ctx.bezierCurveTo(this.model.pmsX + this.model.bezierPipeX1, this.model.bezierPipeY1, this.model.pmsX, this.model.bezierPipeY2, this.model.pmsX + this.model.bezierPipeX3, this.model.bezierPipeY3);
+        this.ctx.lineTo(this.model.pmsX + this.model.bezierPipeX3 + 5, this.model.bezierPipeY3);
+        this.ctx.lineTo(this.model.pmsX + this.model.bezierPipeX3 + 5, this.model.bezierPipeY3 - 15);
+        this.ctx.lineTo(this.model.pmsX + this.model.bezierPipeX3 + 15, this.model.bezierPipeY3 - 15);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();//middle block exterior right
+        this.ctx.lineCap = "butt";
+        this.ctx.moveTo(this.model.rightChamberWall, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.rightChamberWall, this.model.pistonY - this.model.pistonHeight/2 - this.model.bezierPadding1);
+        this.ctx.bezierCurveTo(this.model.rightChamberWall - this.model.bezierPipeX1, this.model.bezierPipeY1, this.model.rightChamberWall, this.model.bezierPipeY2, this.model.rightChamberWall - this.model.bezierPipeX3, this.model.bezierPipeY3-15);
+        this.ctx.lineTo(this.model.rightChamberWall - this.model.bezierPipeX3 - 20, this.model.bezierPipeY3 - 15);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();//middle block exterior left
+        this.ctx.moveTo(this.model.leftChamberWall, this.model.pistonY - this.model.pistonHeight/2);
+        this.ctx.lineTo(this.model.leftChamberWall, this.model.pistonY - this.model.pistonHeight/2 - this.model.bezierPadding1);
+        this.ctx.bezierCurveTo(this.model.leftChamberWall + this.model.bezierPipeX1, this.model.bezierPipeY1, this.model.leftChamberWall, this.model.bezierPipeY2, this.model.leftChamberWall + this.model.bezierPipeX3, this.model.bezierPipeY3-15);
+        this.ctx.lineTo(this.model.leftChamberWall + this.model.bezierPipeX3 + 20, this.model.bezierPipeY3 - 15);
+        this.ctx.stroke();
+    }
 }
 
 
-const piston_h = 120;
-const piston_w = 20;
-let piston_x;   //the value at the middle of the piston
-const piston_y = 560; //height of the piston axis
-const connectingRod_w = 350; //goes into the middle of the piston
-const connectingRod_h = 10;
-const rodLength = 350;
+class SteamModel {
+    constructor(angle, width, height) {
+        this.angle = 0;
+        this.width = width;
+        this.height = height;
+        this.designWidth = 1422;
+        this.scale = this.width / this.designWidth;
 
-function drawPiston(angle){
-    //coords calculation:
-    let wheel_arm_x = wheel_x + wheel_piston_r * Math.cos(angle);
-    let wheel_arm_y = wheel_y + wheel_piston_r*Math.sin(angle);
+        //wheel
+        this.wheelX = 1070;
+        this.wheelY = 600;
+        this.wheelExteriorRadius = 240;
+        this.wheelPistonRadius = 100;
+        this.spokeNumber = 7;
+        this.spokeAngle = (2 * Math.PI) / this.spokeNumber;
+        this.spokeLineWidth = 8;
+        this.spokeCurve = 0.15 * this.wheelExteriorRadius;
 
-    let dy = wheel_arm_y - piston_y;
-    let dx = Math.sqrt(rodLength**2 - dy**2);
-    piston_x = wheel_arm_x - dx - connectingRod_w -  piston_w/2;
+        //piston
+        this.pistonHeight = 120;
+        this.pistonWidth = 20;
+        this.pistonX = undefined;   //the value at the middle of the piston
+        this.pistonY = 560; //height of the piston axis
+        this.pistonRodWidth = 350; //goes into the middle of the piston
+        this.pistonRodHeight = 10;
+        this.pistonRodLength = 350;
 
-    //from the wheel to the piston rod
-    ctx.beginPath();
-    ctx.lineCap = "round";
-    ctx.moveTo(wheel_arm_x, wheel_arm_y);
-    ctx.lineTo(piston_x + connectingRod_w, piston_y);
-    ctx.stroke();
+        //combustion chamber
+        this.combustionChamberWidth = 50;
+        this.rodGap = 8;
+        this.staticCalculus();
+    }
 
-    //fixed rod
-    ctx.beginPath();
-    ctx.fillRect(piston_x, piston_y - connectingRod_h/2, connectingRod_w, connectingRod_h);
-    //piston
-    ctx.fillRect(piston_x - piston_w/2, piston_y - piston_h / 2, piston_w, piston_h); 
-    ctx.fill();
+    calculate(deltaAngle) {
+        this.angle += deltaAngle;
+
+        this.wheelArmX = this.wheelX + this.wheelPistonRadius * Math.cos(this.angle);
+        this.wheelArmY = this.wheelY + this.wheelPistonRadius * Math.sin(this.angle);
+
+        this.pistonRodX = this.wheelArmX - Math.sqrt(this.pistonRodLength**2 - (this.wheelArmY - this.pistonY)**2);
+        this.pistonX = this.pistonRodX - this.pistonRodLength - this.pistonWidth/2;
+    }
+
+    staticCalculus(){
+        let delta_Y = Math.abs(this.wheelY - this.pistonY);
+        this.pmsX = this.wheelX - Math.sqrt((this.pistonRodLength + this.wheelPistonRadius)**2 - delta_Y**2) - this.pistonRodWidth  - this.pistonWidth;
+        this.pmiX = this.wheelX - Math.sqrt((this.pistonRodLength - this.wheelPistonRadius)**2 - delta_Y**2) - this.pistonRodWidth;
+        this.middlePoint = (this.pmiX + this.pmsX)/2;
+
+        this.rightChamberWall = this.pmiX + this.combustionChamberWidth;
+        this.leftChamberWall = this.pmsX - this.combustionChamberWidth;
+
+        this.bezierPadding1 = 20
+        this.bezierPipeX1 = 30;
+        this.bezierPipeY1 = this.pistonY - this.pistonHeight/2 - 40;
+        this.bezierPipeY2 = this.pistonY - this.pistonHeight/2 - 40;
+        this.bezierPipeX3 = 50;
+        this.bezierPipeY3 = this.pistonY - this.pistonHeight/2 - 50;
+
+    }
+
+    resize(width, height) {
+        this.width = width;
+        this.height = height;
+        this.scale = width / this.designWidth;
+        this.staticCalculus();
+    }
 }
 
 
+
+
+
+
+/*
 const combustionChamber_w = 50;
 const combustionPipe_r = combustionChamber_w - piston_w/2 + 5;
 const bezier_pipe_x1_term = 60;
@@ -108,24 +275,8 @@ const bezier_pipe_y1_term = 20;
 const bezier_pipe_y2_term = 40;
 const bezier_pipe_y3_term = 60;
 
-//for excentric rods
-let delta_Y = Math.abs(wheel_y - piston_y);
-let x_pms = Math.sqrt((rodLength - wheel_piston_r)**2 - delta_Y**2);
-let x_pmi = Math.sqrt((rodLength + wheel_piston_r)**2 - delta_Y**2);
-
-let left_wall_combustion_x = wheel_x - x_pmi - connectingRod_w - piston_w/2 - combustionChamber_w;
-let rigth_wall_combustion_x = wheel_x - x_pms - connectingRod_w - piston_w/2 + combustionChamber_w;
 function drawCombustionChamber(){
-    ctx.beginPath(); //lower combustion chamber
-    ctx.moveTo(rigth_wall_combustion_x, piston_y + 10);
-    ctx.lineTo(rigth_wall_combustion_x, piston_y + piston_h/2);
-    ctx.lineTo(left_wall_combustion_x, piston_y + piston_h/2);
-    ctx.lineTo(left_wall_combustion_x, piston_y - piston_h/2);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(rigth_wall_combustion_x, piston_y - 10);
-    ctx.lineTo(rigth_wall_combustion_x, piston_y - piston_h/2);
-    ctx.stroke();
+
 
     ctx.beginPath(); //middle block section
     ctx.moveTo(left_wall_combustion_x + combustionPipe_r, piston_y - piston_h/2);
@@ -273,38 +424,11 @@ function drawExhaust(){
     ctx.lineTo(rigth_wall_combustion_x - bezier_pipe_x2_term + top_chamber_w - exhaust_x - exhaust_w + 112 , piston_y - piston_h/2 - bezier_pipe_y3_term - valve_h - exhaust_h - exhaust_w/2 - 112);
     ctx.stroke();
 }
-
+*/
 
 let lastTime = 0;
 function animate(time){
-    const deltaTime = (time - lastTime) / 1000;
-    lastTime = time;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    const scale = canvas.width / 1422; // original design width
-
-    ctx.save();
-    ctx.scale(scale, scale);
-    ctx.translate(-40, 0);
-    draw(angle);
-    ctx.restore();
-
-    angle += (300 * Math.PI / 180)* deltaTime;
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = linewWidth;
-    requestAnimationFrame(animate);
 }
 
-requestAnimationFrame(animate);
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth / 1.2;
-    canvas.height = canvas.width * 9 / 16;  // keep aspect ratio
-}
-
-window.addEventListener('resize', () => {
-    resizeCanvas();
-});
-resizeCanvas();
